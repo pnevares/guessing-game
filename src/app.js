a1f12184f7ebb6f9d4ae2a0d460166821d93a811
Pablo Nevares <pablo@nevares.com>
2018-07-18 23:50:51 -0700

    Mapping to create the puzzle word status, adding randomWord and randomLetters

--- src/app.js
+++ src/app.js
@@ -2,19 +2,40 @@ import React from 'react';
 import Strikes from './strikes';
 import Word from './word';
 import Letters from './letters';
+import randomWord from './randomWord';
+import randomLetters from './randomLetters';
 
 export default class App extends React.Component {
   constructor() {
     super();
+
+    const word = randomWord();
+
+    this.state = {
+      word,
+      letters: randomLetters(word),
+      strikes: 4,
+      matches: Array(word.length).fill(0),
+    };
+  }
+
+  reset() {
     this.state = {};
   }
 
   render() {
+    const {
+      strikes,
+      word,
+      letters,
+      matches,
+    } = this.state;
+
     return (
-      <div>
-        <Strikes count={1} />
-        <Word />
-        <Letters letters={['A', 'B', 'C', 'D', 'E', 'F']} />
+      <div style={{ fontFamily: 'monospace', fontSize: '2em' }}>
+        <Strikes count={strikes} />
+        <Word word={word} matches={matches} />
+        <Letters letters={letters} />
       </div>
     );
   }
--- src/word.js
+++ src/word.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default () => (
+const Word = ({ word, matches }) => (
   <p>
-    _ _ _ _ _ _
+    {word.split('').map((letter, index) => (
+      <span
+        style={{ paddingRight: '5px' }}
+        key={`${letter}${index}`}
+      >
+        { matches[index] ? letter : '_'}
+      </span>
+    ))}
   </p>
 );
+
+Word.propTypes = {
+  word: PropTypes.string.isRequired,
+  matches: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default Word;
