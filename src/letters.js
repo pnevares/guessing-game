10815fdb26cb37651a7e2b78db17623cd2bbdba3
Pablo Nevares <pablo@nevares.com>
2018-07-18 20:42:40 -0700

    Eslint cleanup, add prop-types

--- src/letters.js
+++ src/letters.js
@@ -1,19 +1,28 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid, no-console */
 
-export default ({ letters }) => (
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Letters = ({ letters }) => (
   <p>
-    {letters.map((letter, index) => (
+    {letters.map(letter => (
       <a
         href="#"
         onClick={(e) => {
           e.preventDefault();
           console.log(`${letter} clicked`);
         }}
-        key={index}
-        style={{paddingRight: '5px'}}
+        key={letter}
+        style={{ paddingRight: '5px' }}
       >
         {letter}
       </a>
     ))}
   </p>
 );
+
+Letters.propTypes = {
+  letters: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Letters;
--- src/strikes.js
+++ src/strikes.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Strikes extends React.Component {
   renderStrikes() {
     const { count } = this.props;
     const strikes = [];
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i += 1) {
       const color = i < count ? '#f00' : '#ccc';
       strikes.push(
         <span
@@ -23,3 +24,7 @@ export default class Strikes extends React.Component {
     return this.renderStrikes();
   }
 }
+
+Strikes.propTypes = {
+  count: PropTypes.number.isRequired,
+};
