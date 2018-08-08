e6a57c88eda548fb45bf25d9beaeca31919d2cfb
Pablo Nevares <pablo@nevares.com>
2018-08-07 22:25:15 -0700

    Added ten more words

--- src/randomWord.js
+++ src/randomWord.js
@@ -2,13 +2,23 @@ export default () => {
   const words = [
     'ABOUND',
     'ADVICE',
+    'ARTIST',
     'ASCEND',
+    'CLASSY',
     'COUPLE',
     'DOWNER',
     'EXEMPT',
+    'KNIGHT',
     'PRAISE',
     'RECIPE',
+    'REALLY',
+    'STARRY',
+    'STATES',
+    'TRACKS',
+    'UNITED',
     'VIABLE',
+    'WEEKLY',
+    'WOLVES',
     'ZOMBIE',
   ];
 
