c99d6af6a964b5a525ed675264b3b79f6ac6bb24
Pablo Nevares <pablo@nevares.com>
2019-01-19 10:15:44 -0700

    Updated components: • Made Strikes a simpler fn component • Separated components from non-component JS • Use React.memo for fn components

---
+++ src/components/Strikes.js
@@ -0,0 +1,25 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+
+const Strikes = memo(({ count }) => {
+  const strikes = [];
+
+  for (let i = 0; i < 6; i += 1) {
+    const status = i < count ? 'wrong' : 'default';
+    strikes.push(
+      <span
+        className={status}
+        key={i}
+      >
+        X
+      </span>,
+    );
+  }
+  return strikes;
+});
+
+Strikes.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+export default Strikes;
