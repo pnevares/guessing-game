3690e3020598ec3c8f27763f79e5501e5fb59455
Pablo Nevares <pablo@nevares.com>
2019-12-09 10:39:46 -0700

    First hooks work

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { useState } from "react";
 import Letters from "./Letters";
 import Strikes from "./Strikes";
 import Word from "./Word";
@@ -6,25 +6,29 @@ import { version } from '../../package.json';
 import randomLetters from "../randomLetters";
 import randomWord from "../randomWord";
 
-export default class App extends PureComponent {
-  state = this.getInitialState();
+const initialState = () => {
+  const word = randomWord();
 
-  getInitialState() {
-    const word = randomWord();
+  return {
+    done: false,
+    letters: randomLetters(word),
+    matches: Array(word.length).fill(false),
+    strikes: 0,
+    usedLetters: [],
+    word
+  };
+};
 
-    return {
-      done: false,
-      letters: randomLetters(word),
-      matches: Array(word.length).fill(false),
-      strikes: 0,
-      usedLetters: [],
-      word
-    };
-  }
+export default function App() {
+  const defaultState = initialState();
+  const [done, setDone] = useState(defaultState.done);
+  const [letters, setLetters] = useState(defaultState.letters);
+  const [matches, setMatches] = useState(defaultState.matches);
+  const [strikes, setStrikes] = useState(defaultState.strikes);
+  const [usedLetters, setUsedLetters] = useState(defaultState.usedLetters);
+  const [word, setWord] = useState(defaultState.word);
 
-  onLetterClick = letter => {
-    const { done, matches, strikes, usedLetters, word } = this.state;
-
+  const onLetterClick = letter => {
     if (done) {
       return;
     }
@@ -47,40 +51,39 @@ export default class App extends PureComponent {
 
     const newDone = newStrikes === 6 || newMatches.filter(c => !c).length === 0;
 
-    this.setState({
-      done: newDone,
-      matches: newMatches,
-      strikes: newStrikes,
-      usedLetters: newUsedLetters
-    });
+    setDone(newDone);
+    setMatches(newMatches);
+      setStrikes(newStrikes);
+      setUsedLetters(newUsedLetters);
   };
 
-  onResetClick = () => {
-    this.setState(this.getInitialState());
+  const reset = () => {
+    setDone(defaultState.done);
+    setLetters(defaultState.letters);
+    setMatches(defaultState.matches);
+    setStrikes(defaultState.strikes);
+    setUsedLetters(defaultState.usedLetters);
+    setWord(defaultState.word);
   };
 
-  render() {
-    const { letters, matches, strikes, usedLetters, word } = this.state;
-
-    return (
-      <div>
-        <h1>
-          Guessing Game v
-          {version}
-        </h1>
-        <Strikes count={strikes} />
-        <Word word={word} matches={matches} />
-        <Letters
-          letters={letters}
-          usedLetters={usedLetters}
-          clickHandler={this.onLetterClick}
-        />
-        <button type="button" onClick={this.onResetClick}>
-          Reset
-        </button>
-        {strikes === 6 && " Game over!"}
-        {matches.filter(c => !c).length === 0 && " Well done!"}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>
+        Guessing Game v
+        {version}
+      </h1>
+      <Strikes count={strikes} />
+      <Word word={word} matches={matches} />
+      <Letters
+        letters={letters}
+        usedLetters={usedLetters}
+        clickHandler={onLetterClick}
+      />
+      <button type="button" onClick={reset}>
+        Reset
+      </button>
+      {strikes === 6 && " Game over!"}
+      {matches.filter(c => !c).length === 0 && " Well done!"}
+    </div>
+  );
 }
